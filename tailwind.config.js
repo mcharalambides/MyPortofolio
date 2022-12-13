88d3759ca86693d24a6f8be6ad3466e400a56bd3
mario <marios-1996@windowslive.com>
2022-12-14 01:49:55 +0200

    Trying to fix styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 
+const purgeGlob = '/**/!(*.stories|*.spec).{ts,tsx}';
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
+  ],
+  purge: [
+    __dirname + purgeGlob,
+    ...createGlobPatternsForDependencies('mcharalambides/MyPortofolio', purgeGlob),
   ],
   theme: {
     extend: {
