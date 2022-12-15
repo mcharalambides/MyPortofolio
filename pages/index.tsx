59a7e5b2f96b8819c6dbcfda25845da7c5361ae2
mario <marios-1996@windowslive.com>
2022-12-16 01:02:24 +0200

    Created Row component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,19 @@ import Head from 'next/head'
 import Image from 'next/image'
 import {AiFillLinkedin, AiFillGithub} from "react-icons/ai"
 
+
+function Row({text1,text2,text3} : {text1:String, text2:String, text3:String}) {
+  return(
+    <div className='basis-1/4 flex-1 object-cover mx-auto shadow-xl rounded-2xl flex flex-col items-center justify-center divide-y p-10 divide-slate-200'>
+    <h1 className='text-center rounded-md bg-slate-100 leading-8 py-3'>{text1}</h1>
+    <h1 className='text-center rounded-md  leading-8 py-2'>{text2}</h1>
+    <h1 className='text-center rounded-md  leading-8 py-2'>{text3}</h1>
+  </div>
+  );
+
+}
+
+
 const Home: NextPage = () => {
   return (
     <div className=' fixed-bottom'>
@@ -18,24 +31,23 @@ const Home: NextPage = () => {
         <div className="font-semibold py-2">Software Engineer</div>
         </div>
 
-        <div className='flex flex-col gap-10 lg:flex-row lg:flex-wrap'>
-          
-        <div className='basis-1/4 flex-1 object-cover mx-auto shadow-xl rounded-2xl flex flex-col items-center justify-center divide-y p-10 divide-slate-200'>
-          <h1 className='text-center rounded-md bg-slate-100 leading-8 py-3'>2016 - 2022 University of Patras</h1>
-          <h1 className='text-center rounded-md  leading-8 py-2'>Computer Engineering and Informatics Department</h1>
-        </div>
+        <div className='flex flex-col gap-10 lg:flex-col lg:flex-wrap'>
 
-        <div className='basis-1/4 flex-1 object-cover mx-auto shadow-xl rounded-2xl flex flex-col items-center justify-center divide-y p-10 divide-slate-200'>
-          <h1 className='text-center rounded-md bg-slate-100 leading-8 py-3'>2014 - 2016 Infantry School for Reserved Officers</h1>
-          <h1 className='text-center rounded-md  leading-8 py-2'>Computer Engineering and Informatics Department</h1>
-        </div>
+          <Row text1 = "2016 - 2022 University of Patras"
+               text2 = "Computer Engineering and Informatics Department"
+               text3 = "GPA: 8.58/10"
+                 />
 
-        <div className='basis-1/4 flex-1 object-cover mx-auto shadow-xl rounded-2xl flex flex-col items-center justify-center divide-y p-10 divide-slate-200'>
-          <h1 className='text-center rounded-md bg-slate-100 leading-8 py-3'>2011 - 2014 Kykkos B' Lyceum</h1>
-          <h1 className='text-center rounded-md  leading-8 py-2'>Computer Engineering and Informatics Department</h1>
-        </div>
+          <Row text1 = "2014 - 2016 Infantry School for Reserved Officers"
+            text2 = "Graduated the Infantry School of ReserveOfficers and completed duties serving as a Reserve Second Lieutenan"
+            text3=""
+            />
 
-        </div>
+          <Row text1 = "2011 - 2014 Kykkos B' Lyceum"
+             text2 = "High School"
+             text3 = "GPA: 19/20"/>
+
+      </div>
 
 
         <div className='text-5xl my-10 flex justify-center gap-8 text-gray-600'>
