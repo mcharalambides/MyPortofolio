08dc93ff6e2e6ecc7e2481ea2ab5c692559e961d
mario <marios-1996@windowslive.com>
2022-12-16 01:10:49 +0200

    Latest Version Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,8 @@ import {AiFillLinkedin, AiFillGithub} from "react-icons/ai"
 
 function Row({text1,text2,text3} : {text1:String, text2:String, text3:String}) {
   return(
-    <div className='basis-1/4 flex-1 object-cover mx-auto shadow-xl rounded-2xl flex flex-col items-center justify-center divide-y p-10 divide-slate-200'>
-    <h1 className='text-center rounded-md bg-slate-100 leading-8 py-3'>{text1}</h1>
+    <div className='w-full basis-1/4 flex-1 object-cover mx-auto shadow-xl rounded-2xl flex flex-col items-center justify-center divide-y p-10 divide-slate-200'>
+    <h1 className='font-semibold w-full text-center rounded-md bg-slate-100 leading-8 py-3'>{text1}</h1>
     <h1 className='text-center rounded-md  leading-8 py-2'>{text2}</h1>
     <h1 className='text-center rounded-md  leading-8 py-2'>{text3}</h1>
   </div>
@@ -39,7 +39,7 @@ const Home: NextPage = () => {
                  />
 
           <Row text1 = "2014 - 2016 Infantry School for Reserved Officers"
-            text2 = "Graduated the Infantry School of ReserveOfficers and completed duties serving as a Reserve Second Lieutenan"
+            text2 = "Graduated the Infantry School of Reserve Officers and completed my military duties serving as a Reserve Second Lieutenan"
             text3=""
             />
 
